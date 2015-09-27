e8c15cfb43159950a4fdfb1edb1fd2de88bbde1c
jliu69 <liu_johnson@yahoo.com>
2015-09-27 01:33:52 -0400

    change files

---
+++ ImgSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImgSearch
+//
+//  Created by Johnson Liu on 9/26/15.
+//  Copyright (c) 2015 HomeOffice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
